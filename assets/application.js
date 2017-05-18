9d8724e1867a5a8f670c3aa70fc393dca65ef31c
Patrick Hagmann <phagmann1@gmail.com>
2017-05-18 19:25:09 +0000

    improved layout framework

--- assets/application.js
+++ assets/application.js
@@ -4,7 +4,7 @@
 // buttonColor changes the color of the box clicked to red, and the previously clicked box back to black
 function buttonColor(curr_div) {
     if (curr_div.id == "all"){
-        curr_div.className = "red-box";
+        curr_div.className = "red-box shapeAllBox";
         document.getElementById("comps").className= "grey-box";
         document.getElementById("typeDevice").innerHTML= "<img src='assets/laptop-icon.svg' height='30px' width='30px'/> All";
         document.getElementById('search').placeholder= "Search All Devices";
@@ -15,7 +15,7 @@ function buttonColor(curr_div) {
     }
     else if(curr_div.id == "comps"){
         curr_div.className = "red-box";
-        document.getElementById("all").className = "grey-box";
+        document.getElementById("all").className = "grey-box shapeAllBox";
         document.getElementById("typeDevice").innerHTML= "<img src='assets/laptop-icon.svg' height='30px' width='30px'/> Laptops";
         document.getElementById('search').placeholder= "Search Laptops";
         document.getElementById('askisk').style.color = "black";
@@ -24,13 +24,15 @@ function buttonColor(curr_div) {
 }
 
 // helper function of 'listDevices' which sorts out the correct html to display on the page
-function htmlForDevice(device,main_div){
+function htmlForDevice(device){
+
+    
     longString = "";
-    longString += "<div class='text-with-device col-md-3'> <img src=" + device.img + " class=" + device.classs + " height=80 width=80 /> ";
+    longString += "<td class='text-with-device'> <img src=" + device.img + " class=" + device.classs + " height=80 width=80 /> ";
     longString += "<div class='device-spacing'>";
     longString += "<h4>"+ device.name +"</h4>";
     if(device.redcircle == true){
-       longString += "<div class='circle'> <medium> ! </medium> </div>";
+        longString += "<div class='circle'> <medium> ! </medium> </div>";
     }
     if(device.newy == true){
         longString += "<div class='gold-box'> <regular> NEW </regular> </div>";
@@ -38,27 +40,44 @@ function htmlForDevice(device,main_div){
     longString += "<div class='num-clicked'> <regular> " + device.number + " </regular> </div>";
     longString += "<p style='color: #999999;'>From $" + device.cost + "/month  </p>";
     longString += "</div>";
-    longString += "</div>";
-    main_div.innerHTML += longString;
+    longString += "</td>";
+    return longString
 }
 // listDevices simply updates the page with the correct devices associated to the respective buttons at the top of the page
 function listDevices(curr_div) {
 
-var main_div = document.getElementById("listing");
-main_div.innerHTML = "";
-var listDevice = [mac, dellLat , mota, dellPre];
+    var main_div = document.getElementById("listing");
+    main_div.innerHTML = "";
+    var rowString = "";
+    var mainString = "";
+    var listDevice = [mac, dellLat , mota, dellPre];
+    var rowCounter = 1;
+    for (deviceIndex in listDevice){
+        device = listDevice[deviceIndex];
+        if(rowCounter % 3 == 1 && rowCounter > 1){
+            rowString = "<tr>" + rowString + "</tr>";
+            mainString += rowString;
+            rowString = "";
+        }
+        if(device.comps == true && curr_div.id == "comps"){
+            rowString += htmlForDevice(device);
+            rowCounter += 1;
 
-for (deviceIndex in listDevice){
-    device = listDevice[deviceIndex];
-    if(device.comps == true && curr_div.id == "comps"){
-        htmlForDevice(device,main_div);
+        }
+        else if(curr_div.id == "all"){
+            rowString += htmlForDevice(device);
+            rowCounter += 1;
 
+        }
+        
     }
-    else if(curr_div.id == "all"){
-        htmlForDevice(device,main_div);
+    if(rowString != ""){
+            rowString = "<tr>" + rowString + "</tr>";
+            mainString += rowString;
+            rowString = "";
 
-    }
-}
+        }
+    main_div.innerHTML += mainString;
 
 }
 // searchUpdate updates the devices that are associated with what is currently typed in the search bar. 
@@ -70,23 +89,45 @@ function searchUpdate(search){
     var main_div = document.getElementById("listing");
     main_div.innerHTML = "";
     var listDevice = [mac, dellLat , mota, dellPre];
+    var rowCounter = 0;
+    var rowString = "";
+    var mainString = "";
     if(all_div.className == "red-box"){
         for (deviceIndex in listDevice){
             device = listDevice[deviceIndex];
+            if(rowCounter % 3 == 1 && rowCounter > 1){
+                rowString = "<tr>" + rowString + "</tr>";
+                mainString += rowString;
+                rowString = "";
+            }
             if(device.name.toLowerCase().startsWith(search) == true){
-                htmlForDevice(device,main_div);
+                rowString += htmlForDevice(device);
+                rowCounter += 1;
             }
         }
     }
     else{
         for (deviceIndex in listDevice){
-            device = listDevice[deviceIndex]
-            if(device.name.toLowerCase().startsWith(search) == true && device.comps == true){
-                htmlForDevice(device,main_div);
+            device = listDevice[deviceIndex];
+            if(rowCounter % 3 == 1 && rowCounter > 1){
+                rowString = "<tr>" + rowString + "</tr>";
+                mainString += rowString;
+                rowString = "";
             }
+            if(device.name.toLowerCase().startsWith(search) == true && device.comps == true){
+                rowString += htmlForDevice(device);
+                rowCounter += 1;
+            }
+            
         } 
     }
+    if(rowString != ""){
+            rowString = "<tr>" + rowString + "</tr>";
+            mainString += rowString;
+            rowString = "";
 
+        }
+    main_div.innerHTML += mainString;
 }
 
 // JSON data of each device
